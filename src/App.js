a0d3027f792f04a679de9fdede198b51ab800008
Yolo <Yolo051@outlook.es>
2023-04-17 11:29:42 -0600

    updateinfor

--- src/App.js
+++ src/App.js
@@ -16,17 +16,33 @@ function App() {
   }
   return (
     <div className="App">
+       <div class="character-container1">
+            <div>
+           <img src={imagenmifoto} alt="Yolotzin" className="img-home1"/>
+           </div>
+           <div >
+           <center >
+            <h1>Yolotzin Groth Hernandez</h1>
+           <p>zS20020311</p>
+           <p>Software engineering student & photographer</p>
+           <p>Yolo051@outlook.es</p> <p>IG:@groth.yolo</p>
+           </center>
+           </div>
+           </div>
       <header className="App-header">
         <h1 className="title"> Rick & Morty</h1>
         {characters? (
           <Characters characters={characters} setCharacters={setCharacters}/>
         ):(
           <>
+          
            <img src={imagenRickMorty} alt="Rick & Morty" className="img-home"/>
            <button onClick={reqApi} className="btn-search">
            Buscar personajes
            </button>
+          
           </>
+
         )
         }
       </header>
@@ -34,9 +50,7 @@ function App() {
     
   );
 
-  <div>
- <img src={imagenmifoto} alt="mifoto" className="img-home"/>
-  </div>
+ 
 
 }
 
